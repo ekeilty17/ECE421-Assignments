3a26ba8478b54f25558e9998fd2a017579008c99
DRanade <github@devansh.sent.com>
2020-01-27 14:53:58 -0500

    Added bias term in gradient calculation

--- A1/starter.py
+++ A1/starter.py
@@ -1,21 +1,22 @@
 from warnings import simplefilter
-# ignore all future warnings
-simplefilter(action='ignore', category=FutureWarning)
-
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
+
+
 def loadData():
-    with np.load('notMNIST.npz') as data :
+    with np.load('notMNIST.npz') as data:
         Data, Target = data['images'], data['labels']
         posClass = 2
         negClass = 9
-        dataIndx = (Target==posClass) + (Target==negClass)
-        Data = Data[dataIndx]/255.
+        dataIndx = (Target == posClass) + (Target == negClass)
+        Data = Data[dataIndx] / 255.
         Target = Target[dataIndx].reshape(-1, 1)
-        Target[Target==posClass] = 1
-        Target[Target==negClass] = 0
+        Target[Target == posClass] = 1
+        Target[Target == negClass] = 0
         np.random.seed(421)
         randIndx = np.arange(len(Data))
         np.random.shuffle(randIndx)
@@ -25,28 +26,37 @@ def loadData():
         testData, testTarget = Data[3600:], Target[3600:]
     return trainData, validData, testData, trainTarget, validTarget, testTarget
 
+
 # reg = lambda
 # I'm assuming x is the matrix version
-def MSE(W, b, x, y, reg):
-    return np.linalg.norm(x.dot(W) - y)**2 + reg * np.linalg.norm(w)**2
+# I agree with this interpretation, but I changed input W to w since it's a vector and not matrix -Dev
+def MSE(w, b, x, y, reg):
+    return np.linalg.norm(x.dot(w) + b - y) ** 2 + reg / 2 * np.linalg.norm(w) ** 2
 
-def gradMSE(W, b, x, y, reg):
-    return 2 * ( x.T.dot( x.dot(W) - y ) + reg * W )
 
-def grad_descent(W, b, x, y, alpha, epochs, reg, error_tol):
+# Je suis confused. The document says 'return the gradient with respect to the weights and the gradient with respect to
+#   the bias. Which one do we return? Or, are we supposed to return both in a list or something?
+def gradMSE(w, b, x, y, reg):
+    return 2 * (x.T.dot(x.dot(w) + b - y) + reg * w)
+
+
+def grad_descent(w, b, x, y, alpha, epochs, reg, error_tol):
     # Your implementation here
     pass
+
 
 def crossEntropyLoss(W, b, x, y, reg):
     # Your implementation here
     pass
 
+
 def gradCE(W, b, x, y, reg):
     # Your implementation here
     pass
 
+
 def buildGraph(loss="MSE"):
-    #Initialize weight and bias tensors
+    # Initialize weight and bias tensors
     tf.set_random_seed(421)
 
     if loss == "MSE":
@@ -54,8 +64,10 @@ def buildGraph(loss="MSE"):
         pass
 
     elif loss == "CE":
-        #Your implementation here
+        # Your implementation here
         pass
 
+
 if __name__ == "__main__":
-    #trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
+    trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
+    print("hi")
