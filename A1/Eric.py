364fc1b0211c7effaa03792e1048f8bd24ab4ae3
Eric <epkeilty@hotmail.com>
2020-01-28 16:19:33 -0500

    Add Eric file

---
+++ A1/Eric.py
@@ -0,0 +1,168 @@
+# ignore all future warnings
+from warnings import simplefilter
+simplefilter(action='ignore', category=FutureWarning)
+
+# imports
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# given by the assignment
+def loadData():
+    with np.load('notMNIST.npz') as data :
+        Data, Target = data['images'], data['labels']
+        posClass = 2
+        negClass = 9
+        dataIndx = (Target==posClass) + (Target==negClass)
+        Data = Data[dataIndx]/255.
+        Target = Target[dataIndx].reshape(-1, 1)
+        Target[Target==posClass] = 1
+        Target[Target==negClass] = 0
+        np.random.seed(421)
+        randIndx = np.arange(len(Data))
+        np.random.shuffle(randIndx)
+        Data, Target = Data[randIndx], Target[randIndx]
+        trainData, trainTarget = Data[:3500], Target[:3500]
+        validData, validTarget = Data[3500:3600], Target[3500:3600]
+        testData, testTarget = Data[3600:], Target[3600:]
+    return trainData, validData, testData, trainTarget, validTarget, testTarget
+
+trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
+"""
+print(f"Training Data: {trainData.shape}\tTraining tagets: {trainTarget.shape}")
+print(f"Validation Data: {validData.shape}\tValidation tagets: {validTarget.shape}")
+print(f"Testing Data: {testData.shape}\tTesting tagets:{testTarget.shape}")
+"""
+
+"""
+        Helpful Functions
+"""
+def plot(image, target):
+    plt.imshow(image, cmap="hot")
+    plt.title('J' if target == 0 else 'C')
+    # targets are binary encoded 0 == 'J' and 1 == 'C'
+    plt.show()
+
+def augment(X, w, b):
+    # flatten X
+    if len(X.shape) == 3:
+        X = X.reshape(X.shape[0], -1)
+    
+    # insert 1's at position 0 along the columns
+    X = np.insert(X, 0, 1, axis=1)
+    
+    # insert b at the front of W
+    w = np.insert(w, 0, b, axis=0)
+    
+    return X, w
+
+def predict(w, b, X):
+    X = X.reshape(X.shape[0], -1)
+    return X.dot(w) + b
+
+def accuracy(w, b, X, y):
+    y = y.reshape(-1)
+    y_pred = predict(w, b, X)
+    y_pred = np.vectorize(lambda z: 1 if z > 0 else 0)(y_pred)
+    return sum(y_pred == y) / y.shape[0]
+
+def plot_loss(x, train_loss=None, valid_loss=None, test_loss=None, title=None):
+    if train_loss != None:
+        plt.plot(x, train_loss, label="Training Loss")
+    if valid_loss != None:
+        plt.plot(x, valid_loss, label="Validation Loss")
+    if test_loss != None:
+        plt.plot(x, test_loss, label="Testing Loss")
+    
+    if title == None:
+        plt.title("Training Loss")
+    else:
+        plt.title(title)
+    
+    plt.xlabel("Epochs")
+    plt.xlim(left=0)
+    plt.ylabel("Loss")
+    plt.legend(loc="upper right")
+    plt.show()
+
+def plot_accuracy(x, train_accuracy=None, valid_accuracy=None, test_accuracy=None, title=None):
+    if train_accuracy != None:
+        plt.plot(x, train_accuracy, label="Training Accuracy")
+    if valid_accuracy != None:
+        plt.plot(x, valid_accuracy, label="Validation Accuracy")
+    if test_accuracy != None:
+        plt.plot(x, test_accuracy, label="Testing Accuracy")
+    
+    if title == None:
+        plt.title("Accuracy")
+    else:
+        plt.title(title)
+
+    plt.xlabel("Epochs")
+    plt.xlim(left=0)
+    plt.ylabel("Accuracy")
+    plt.yticks(np.arange(0, 1.1, step=0.1))
+    plt.grid(linestyle='-', axis='y')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+"""
+        MSE Loss
+"""
+def MSE(w, b, X, y, reg):
+    X = X.reshape(X.shape[0], -1)
+    y = y.reshape(-1)
+    return np.square(X.dot(w) + b - y).mean() + reg * np.square(w).sum()
+
+def gradMSE(w, b, X, y, reg):
+    X = X.reshape(X.shape[0], -1)
+    y = y.reshape(-1)
+    N = y.shape[0]
+    
+    w_grad = 2.0/N * X.T.dot(X.dot(w) + b - y) + reg * w
+    b_grad = 2.0/N * np.sum(X.dot(w) + b - y)
+    return w_grad, b_grad
+
+def grad_descent_MSE(w, b, X, y, alpha, epochs, reg, error_tol, validData=None, validTarget=None, testData=None, testTarget=None):
+    train_loss, train_acc = [], []
+    valid_loss, valid_acc = [], []
+    test_loss, test_acc = [], []
+    printing = True
+    for i in range(epochs):
+        grad_w, grad_b = gradMSE(w, b, X, y, reg)
+        w -= alpha * grad_w
+        b -= alpha * grad_b
+        
+        # Calculating Statistics
+        train_loss.append( MSE(w, b, X, y, reg) )
+        train_acc.append( accuracy(w, b, X, y) )
+
+        if validData != None and validTarget != None:
+            valid_loss.append( MSE(w, b, validData, validTarget, reg) )
+            valid_acc.append( accuracy(w, b, validData, validTarget) )
+        if testData != None and testTarget != None:
+            test_loss.append( MSE(w, b, testData, testTarget, reg) )
+            valid_acc.append( accuracy(w, b, testData, testTarget) )
+        
+        # Print Losses and Accurancies if printing is on
+        if printing:
+            print(f"Training loss: {train_loss[-1]:.4f}\tTraining acc: {train_acc[-1]*100:.2f}%")
+            if validData != None and validTarget != None:
+                print(f"Validation loss: {valid_loss[-1]:.4f}\tValidation acc: {valid_acc[-1]*100:.2f}%")
+            if testData != None and testTarget != None:
+                print(f"Testing loss: {test_loss[-1]:.4f}\tTesting acc: {test_acc[-1]*100:.2f}%")
+
+        # Check stopping condition
+        if i > 1 and np.abs(train_loss[-2] - train_loss[-1]) <= error_tol:
+            break
+
+    statistics = (train_loss, train_acc)
+    if validData != None and validTarget != None:
+        statistics += (valid_loss, valid_acc, )
+    if testData != None and testTarget != None:
+        statistics += (test_loss, test_acc,)
+    # Python 3.8 made this easier, but 3.7 you have to do this
+    out = (w, b, *statistics)
+    
+    return out
