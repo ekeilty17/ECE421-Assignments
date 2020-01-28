07fd2b33640f7b1e96ff427356b158e368256c77
Eric <epkeilty@hotmail.com>
2020-01-28 17:14:43 -0500

    3. Tuning the Learning Rate

--- A1/Eric.py
+++ A1/Eric.py
@@ -66,7 +66,9 @@ def accuracy(w, b, X, y):
     y_pred = np.vectorize(lambda z: 1 if z > 0 else 0)(y_pred)
     return sum(y_pred == y) / y.shape[0]
 
-def plot_loss(x, train_loss=None, valid_loss=None, test_loss=None, title=None):
+def plot_loss(x, train_loss=None, valid_loss=None, test_loss=None, title=None, subplot=False):
+    if subplot:
+        plt.subplot(1, 2, 1)
     if train_loss != None:
         plt.plot(x, train_loss, label="Training Loss")
     if valid_loss != None:
@@ -75,7 +77,7 @@ def plot_loss(x, train_loss=None, valid_loss=None, test_loss=None, title=None):
         plt.plot(x, test_loss, label="Testing Loss")
     
     if title == None:
-        plt.title("Training Loss")
+        plt.title("Loss")
     else:
         plt.title(title)
     
@@ -83,9 +85,10 @@ def plot_loss(x, train_loss=None, valid_loss=None, test_loss=None, title=None):
     plt.xlim(left=0)
     plt.ylabel("Loss")
     plt.legend(loc="upper right")
-    plt.show()
 
-def plot_accuracy(x, train_accuracy=None, valid_accuracy=None, test_accuracy=None, title=None):
+def plot_accuracy(x, train_accuracy=None, valid_accuracy=None, test_accuracy=None, title=None, subplot=False):
+    if subplot:
+        plt.subplot(1, 2, 2)
     if train_accuracy != None:
         plt.plot(x, train_accuracy, label="Training Accuracy")
     if valid_accuracy != None:
@@ -104,18 +107,17 @@ def plot_accuracy(x, train_accuracy=None, valid_accuracy=None, test_accuracy=Non
     plt.yticks(np.arange(0, 1.1, step=0.1))
     plt.grid(linestyle='-', axis='y')
     plt.legend(loc="lower right")
-    plt.show()
 
 
 """
         MSE Loss
 """
-def MSE(w, b, X, y, reg):
+def MSE(w, b, X, y, reg=0):
     X = X.reshape(X.shape[0], -1)
     y = y.reshape(-1)
     return np.square(X.dot(w) + b - y).mean() + reg * np.square(w).sum()
 
-def gradMSE(w, b, X, y, reg):
+def gradMSE(w, b, X, y, reg=0):
     X = X.reshape(X.shape[0], -1)
     y = y.reshape(-1)
     N = y.shape[0]
@@ -124,12 +126,16 @@ def gradMSE(w, b, X, y, reg):
     b_grad = 2.0/N * np.sum(X.dot(w) + b - y)
     return w_grad, b_grad
 
+
+"""
+        GD with MSE
+"""
 def grad_descent_MSE(w, b, X, y, alpha, epochs, reg, error_tol, validData=None, validTarget=None, testData=None, testTarget=None):
     train_loss, train_acc = [], []
     valid_loss, valid_acc = [], []
     test_loss, test_acc = [], []
     printing = True
-    for i in range(epochs):
+    for e in range(epochs):
         grad_w, grad_b = gradMSE(w, b, X, y, reg)
         w -= alpha * grad_w
         b -= alpha * grad_b
@@ -138,11 +144,96 @@ def grad_descent_MSE(w, b, X, y, alpha, epochs, reg, error_tol, validData=None, 
         train_loss.append( MSE(w, b, X, y, reg) )
         train_acc.append( accuracy(w, b, X, y) )
 
-        if validData != None and validTarget != None:
+        if not validData is None and not validTarget is None:
             valid_loss.append( MSE(w, b, validData, validTarget, reg) )
             valid_acc.append( accuracy(w, b, validData, validTarget) )
-        if testData != None and testTarget != None:
+        if not testData is None and not testTarget is None:
             test_loss.append( MSE(w, b, testData, testTarget, reg) )
+            test_acc.append( accuracy(w, b, testData, testTarget) )
+        
+        # Print Losses and Accurancies if printing is on
+        if printing:
+            print(f"Training loss: {train_loss[-1]:.4f}\tTraining acc: {train_acc[-1]*100:.2f}%")
+            if not validData is None and not validTarget is None:
+                print(f"Validation loss: {valid_loss[-1]:.4f}\tValidation acc: {valid_acc[-1]*100:.2f}%")
+            if not testData is None and not testTarget is None:
+                print(f"Testing loss: {test_loss[-1]:.4f}\tTesting acc: {test_acc[-1]*100:.2f}%")
+
+        # Check stopping condition
+        if e > 1 and np.abs(train_loss[-2] - train_loss[-1]) <= error_tol:
+            break
+
+    statistics = (train_loss, train_acc)
+    if not validData is None and not validTarget is None:
+        statistics += (valid_loss, valid_acc, )
+    if not testData is None and not testTarget is None:
+        statistics += (test_loss, test_acc,)
+    # Python 3.8 made this easier, but 3.7 you have to do this
+    out = (w, b, *statistics)
+    
+    return out
+
+# 3. Tuning the Learning Rate
+N = trainData.shape[0]
+d = trainData.shape[1] * trainData.shape[2]
+for alpha in [0.005, 0.001, 0.0001]:
+    w = np.random.random_sample(d)
+    b = np.random.random_sample(1)
+    w, b, *statistics = grad_descent_MSE(w, b, trainData, trainTarget, 0.005, 5000, 0, 0.01, validData, validTarget, testData, testTarget)
+    train_loss, train_acc, valid_loss, valid_acc, test_loss, test_acc = statistics
+    plot_loss(np.arange(0, len(train_loss), 1), train_loss, valid_loss, test_loss, subplot=True)
+    plot_accuracy(np.arange(0, len(train_loss), 1), train_acc, valid_acc, test_acc, subplot=True)
+    plt.show()
+    plt.clf()
+
+"""
+        CE Loss
+"""
+def sigmoid(z):
+    return 1.0 / (1 + np.exp(-z))
+
+def crossEntropyLoss(w, b, X, y, reg=0):
+    
+    X, w = augment(X, w, b)
+    
+    y_hat = sigmoid(x.dot(W) + b)
+    L = np.vectorize(lambda x,y: -np.log(x) if y == 1 else -np.log(1-x))(y_hat, y)
+    #... not finished
+
+def gradCE(w, b, X, y, reg=0):
+    # Your implementation here
+    pass
+
+"""
+        GD with either MSE or CE
+"""
+def grad_descent(W, b, x, y, alpha, epochs, reg, error_tol, lossType="MSE"):
+    loss_func, grad_func = None, None
+    if lossType == "MSE":
+        loss_func, grad_func = MSE, gradMSE
+    elif lossType == "CE":
+        loss_func, grad_func = crossEntropyLoss, gradCE
+    else:
+        raise ValueError("Variable 'lossType' must be either 'MSE' or 'CE'.")
+
+    train_loss, train_acc = [], []
+    valid_loss, valid_acc = [], []
+    test_loss, test_acc = [], []
+    printing = True
+    for e in range(epochs):
+        grad_w, grad_b = grad_func(w, b, X, y, reg)
+        w -= alpha * grad_w
+        b -= alpha * grad_b
+        
+        # Calculating Statistics
+        train_loss.append( loss_func(w, b, X, y, reg) )
+        train_acc.append( accuracy(w, b, X, y) )
+
+        if validData != None and validTarget != None:
+            valid_loss.append( loss_func(w, b, validData, validTarget, reg) )
+            valid_acc.append( accuracy(w, b, validData, validTarget) )
+        if testData != None and testTarget != None:
+            test_loss.append( loss_func(w, b, testData, testTarget, reg) )
             valid_acc.append( accuracy(w, b, testData, testTarget) )
         
         # Print Losses and Accurancies if printing is on
@@ -154,7 +245,7 @@ def grad_descent_MSE(w, b, X, y, alpha, epochs, reg, error_tol, validData=None, 
                 print(f"Testing loss: {test_loss[-1]:.4f}\tTesting acc: {test_acc[-1]*100:.2f}%")
 
         # Check stopping condition
-        if i > 1 and np.abs(train_loss[-2] - train_loss[-1]) <= error_tol:
+        if e > 1 and np.abs(train_loss[-2] - train_loss[-1]) <= error_tol:
             break
 
     statistics = (train_loss, train_acc)
@@ -166,3 +257,17 @@ def grad_descent_MSE(w, b, X, y, alpha, epochs, reg, error_tol, validData=None, 
     out = (w, b, *statistics)
     
     return out
+
+def buildGraph(loss="MSE"):
+    #Initialize weight and bias tensors
+    tf.set_random_seed(421)
+
+    loss_func, grad_func = None, None
+    if loss == "MSE":
+        loss_func, grad_func = MSE, gradMSE
+    elif loss == "CE":
+        loss_func, grad_func = crossEntropyLoss, gradCE
+    else:
+        raise ValueError("Variable 'loss' must be either 'MSE' or 'CE'.")
+
+    
